e7f9a6162a8f78bfb3d071c0cd1328a0d3d92973
Jim Barrows <Jim.Barrows@gmail.com>
2020-05-26 18:19:34 -0500

    Initial commit into repo

---
+++ src/main/java/org/erpmicroservices/products/api/rest/ProductsApiRestApplication.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.products.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApiRestApplication {
+
+ public static void main(String[] args) {
+	SpringApplication.run(ProductsApiRestApplication.class, args);
+ }
+
+}
